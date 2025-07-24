c4e388b0dc8dff62ce7d629fc683c97971b87028
Shraddha Tiwari <shraddhatiwari345@gmail.com>
2025-07-24 18:31:28 +0530

    Added Emotion Detection module with preprocessing, model loader, and predictor

---
+++ ml_modules/speech_analysis/sentiment_analyzer.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob
+
+def get_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+
+    if polarity > 0.2:
+        return "Positive", polarity
+    elif polarity < -0.2:
+        return "Negative", polarity
+    else:
+        return "Neutral", polarity
