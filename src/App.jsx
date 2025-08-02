361b26d92a99c3eebed3c5bc48c3a390b9179533
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-08-02 23:02:40 +0530

    Added static jobs to the job section

--- src/App.jsx
+++ src/App.jsx
@@ -104,9 +104,9 @@ const AppWrapper = () => {
             <Route
               path="/dashboard"
               element={
-                <ProtectedRoute>
+                // <ProtectedRoute>
                   <DashboardLayout />
-                </ProtectedRoute>
+                // {/* </ProtectedRoute> */}
               }
             >
               <Route index element={<Dashboard />} />
